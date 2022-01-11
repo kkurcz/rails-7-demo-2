3d03ee45eed3ada0dbafcbd718a06f61baf5fd79
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2022-01-11 23:17:35 +0100

    add posts and comments html

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: %i[ show edit update destroy ]
+  before_action :set_post
 
   # GET /comments or /comments.json
   def index
@@ -21,17 +21,8 @@ class CommentsController < ApplicationController
 
   # POST /comments or /comments.json
   def create
-    @comment = Comment.new(comment_params)
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to comment_url(@comment), notice: "Comment was successfully created." }
-        format.json { render :show, status: :created, location: @comment }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
+    @post.comments.create! params.required(:comment).permit(:content)
+    redirect_to @post
   end
 
   # PATCH/PUT /comments/1 or /comments/1.json
@@ -59,6 +50,11 @@ class CommentsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
+
+    def set_post
+      @post = Post.find(params[:post_id])      
+    end
+    
     def set_comment
       @comment = Comment.find(params[:id])
     end
