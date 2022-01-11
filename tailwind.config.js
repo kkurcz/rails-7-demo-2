88140ec00eab12a9ab71c55de8d4d420d0aa0209
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2022-01-11 17:39:47 +0100

    add new post

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./app/**/*.{html,js,erb}"],
+  theme: {
+    extend: {
+      minHeight: {
+        '20vh': '20vh',
+        '50vh': '50vh',
+      }
+    },
+  },
+  plugins: [],
+}
